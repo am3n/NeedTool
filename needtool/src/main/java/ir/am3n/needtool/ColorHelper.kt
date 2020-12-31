8ae9a29d0f7ce2175c43a91b287118110f33a5a8
Amirhosein Barati <amirhoseinbarati75@gmail.com>
2020-12-31 10:55:04 +0330

    Added int to rgb array

--- needtool/src/main/java/ir/am3n/needtool/ColorHelper.kt
+++ needtool/src/main/java/ir/am3n/needtool/ColorHelper.kt
@@ -1,6 +1,11 @@
 package ir.am3n.needtool
 
-import android.graphics.Color
-import android.graphics.ColorSpace
 import androidx.annotation.ColorInt
 
+val Int.toRgb: IntArray get() {
+    val rgb = IntArray(3)
+    rgb[0] = (this shr 16 and 0xFF)//.toByte()
+    rgb[1] = (this shr  8 and 0xFF)//.toByte()
+    rgb[2] = (this        and 0xFF)//.toByte()
+    return rgb
+}
