c42464aa5fc2d37f8fd2bcdc3f8d6cf4588001b4
Amirhosein Barati <amirhoseinbarati75@gmail.com>
2021-01-04 23:18:59 +0330

    Added EditText OnDone Listener

---
+++ needtool/src/main/java/ir/am3n/needtool/EdittextHelper.kt
@@ -0,0 +1,19 @@
+package ir.am3n.needtool
+
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+
+interface EdittextOnDone {
+    fun callback(): Boolean
+}
+
+fun EditText.onDone(edittextOnDone: EdittextOnDone) {
+    imeOptions = EditorInfo.IME_ACTION_DONE
+    maxLines = 1
+    setOnEditorActionListener { _, actionId, _ ->
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            return@setOnEditorActionListener edittextOnDone.callback()
+        }
+        return@setOnEditorActionListener false
+    }
+}
