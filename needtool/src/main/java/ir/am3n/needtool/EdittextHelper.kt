1898ff448bdd8e11c5e5be793b22fe955b92b57e
Amirhosein <amirhoseinbarati75@gmail.com>
2022-05-08 22:35:43 +0430

    added edittext onSearch method

--- needtool/src/main/java/ir/am3n/needtool/EdittextHelper.kt
+++ needtool/src/main/java/ir/am3n/needtool/EdittextHelper.kt
@@ -5,31 +5,55 @@ import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 
-interface EdittextOnDone {
+interface EdittextOnAction {
     fun callback(): Boolean
 }
 
-fun EditText.onDone(edittextOnDone: EdittextOnDone) {
+fun EditText.onDone(edittextOnAction: EdittextOnAction) {
     imeOptions = EditorInfo.IME_ACTION_DONE
     maxLines = 1
     setOnEditorActionListener { _, actionId, _ ->
         if (actionId == EditorInfo.IME_ACTION_DONE) {
-            return@setOnEditorActionListener edittextOnDone.callback()
+            return@setOnEditorActionListener edittextOnAction.callback()
         }
         return@setOnEditorActionListener false
     }
 }
 
-fun EditText.onDone(edittextOnDone: () -> Boolean) {
+fun EditText.onDone(edittextOnAction: () -> Boolean) {
     imeOptions = EditorInfo.IME_ACTION_DONE
     maxLines = 1
     setOnEditorActionListener { _, actionId, _ ->
         if (actionId == EditorInfo.IME_ACTION_DONE) {
-            return@setOnEditorActionListener edittextOnDone()
+            return@setOnEditorActionListener edittextOnAction()
         }
         return@setOnEditorActionListener false
     }
 }
+
+
+fun EditText.onSearch(edittextOnAction: EdittextOnAction) {
+    imeOptions = EditorInfo.IME_ACTION_SEARCH
+    maxLines = 1
+    setOnEditorActionListener { _, actionId, _ ->
+        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            return@setOnEditorActionListener edittextOnAction.callback()
+        }
+        return@setOnEditorActionListener false
+    }
+}
+
+fun EditText.onSearch(edittextOnAction: () -> Boolean) {
+    imeOptions = EditorInfo.IME_ACTION_SEARCH
+    maxLines = 1
+    setOnEditorActionListener { _, actionId, _ ->
+        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            return@setOnEditorActionListener edittextOnAction()
+        }
+        return@setOnEditorActionListener false
+    }
+}
+
 
 
 fun EditText.showSoftKeyboard() {
